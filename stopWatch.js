240a25b50e417547d93f2461a95e321ebbbaae9e
ycarissan <ycarissan@gmail.com>
2021-04-19 14:03:35 +0200

    correction ms

--- stopWatch.js
+++ stopWatch.js
@@ -63,7 +63,9 @@ var running = 0;function startTimer(){
   var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);hours = (hours < 10) ? "0" + hours : hours;
+//  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
+  var milliseconds = difference - (seconds - minutes * 60 - hours * 3600 ) * 1000
+  hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
   milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
